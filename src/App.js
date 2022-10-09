e4910b9e816db97e962b28e5bfcfcdac282871b2
AcidRainFckU <matweev.ilja@yandex.ru>
2022-10-09 11:32:28 +0300

    remark8

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onToggle={onToggleButton}>toggle</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
